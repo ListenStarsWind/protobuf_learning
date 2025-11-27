95482e173054cd3b098460e3a1783e556f22cba8
听风若依 <wind2106631469@gmail.com>
2025-11-27 21:54:42 +0800

    网络通信录

--- contacts4.0/src/client/main.cc
+++ contacts4.0/src/client/main.cc
@@ -1,22 +1,146 @@
+#include <exception>
+#include <format>
 #include <iostream>
+#include <string>
 
 #include "httplib.h"
+#include "proto/add_contact.pb.h"
 
 using namespace std;
 using namespace httplib;
 
+void menu() {
+    cout << "-------------------------------------" << endl;
+    cout << "----------选择对应的通讯录操作----------" << endl;
+    cout << "------------ 1. 新增联系人 ------------" << endl;
+    cout << "------------ 2. 删除联系人 ------------" << endl;
+    cout << "---------- 3. 查看联系人列表 ----------" << endl;
+    cout << "-------- 4. 查看联系人详细信息 --------" << endl;
+    cout << "-------------- 0. 退出--------------" << endl;
+    cout << "-------------------------------------" << endl;
+}
+
+enum option_type {
+    QUIT_CONTACT = 0,
+    ADD_CONTACT = 1,
+    DELETE_CONTACT = 2,
+    FIND_CONTACTS = 3,
+    FIND_CONTACT_DETAIL = 4
+};
+
+class ContactsException : public exception {
+   public:
+    ContactsException(std::string m) : message(m) {}
+
+    const char* what() const noexcept override {
+        return message.c_str();
+    }
+
+   private:
+    std::string message;
+};
+
+void AddContact();
+
+// 短连接模式
 int main() {
-    Client client("127.0.0.1", 8180);  // 本地回环
-
-    Result res1 = client.Post("/test-post");
-    if (res1->status == 200) {
-        cout << "调用post成功!" << endl;
+    try {
+        menu();
+        cout << "----> 请选择: ";
+        int option = 0;
+        cin >> option;
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        switch (option) {
+            case option_type::QUIT_CONTACT:
+                cout << "-----程序退出-----" << endl;
+                break;
+            case option_type::ADD_CONTACT:
+                AddContact();
+                break;
+            case option_type::DELETE_CONTACT:
+                cout << "这个选项没写, 以后再来探索吧" << endl;
+                break;
+            case option_type::FIND_CONTACTS:
+                cout << "这个选项没写, 以后再来探索吧" << endl;
+                break;
+            case option_type::FIND_CONTACT_DETAIL:
+                cout << "这个选项没写, 以后再来探索吧" << endl;
+                break;
+            default:
+                cout << "如果这是图形化界面, 我根本不会让你有机会来这里!" << endl;
+                break;
+        }
+    } catch (ContactsException& e) {
+        cerr << format("客户端应用层异常: {}\n", e.what());
     }
-
-    Result res2 = client.Get("/test-get");
-    if (res2->status == 200) {
-        cout << "调用get成功!" << endl;
-    }
-
     return 0;
+}
+
+// 收集联系人参数
+void collectContactInfo(add_contact::AddContactRequest& req) {
+    cout << "开始添加新联系人: \n";
+    cout << "请依据提示, 依次输入新联系人的姓名, 年龄, 联系电话 \n";
+
+    uint32_t age;
+    string name, phone;
+
+    cout << "请输入新联系人的姓名: ";
+    getline(cin, name);
+
+    cout << "请输入新联系人的年龄: ";
+    cin >> age;
+
+    // 一直读取整型与换行之前的杂项内容, 包括换行本身
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    cout << "请输入新联系人的电话号码, 支持多个号码保存, 如果确认, 请直接回车\n";
+    for (int i = 0;; ++i) {
+        cout << format("第{}个: ", i + 1);
+        getline(cin, phone);
+        if (phone.empty()) break;
+        req.add_phones(phone);
+    }
+
+    req.set_name(name);
+    req.set_age(age);
+
+    cout << "正在打包收集信息并发送\n";
+}
+
+void AddContact() {
+    // 构造并序列化请求负载
+    add_contact::AddContactRequest req_body;
+    collectContactInfo(req_body);
+    string body;
+    if (!req_body.SerializeToString(&body)) {
+        throw ContactsException("请求序列化失败!");
+    }
+
+    // 进行 post 调用
+    Client c("127.0.0.1", 8180);  // 方便起见, 我们就用本地回环了
+    auto res = c.Post("/contacts/add", body, "application/protobuf");
+    if (!res) {
+        // 会话层出现了错误
+        throw ContactsException(
+            format("/contacts/add 资源请求失败: {}", httplib::to_string(res.error())));
+    }
+
+    // 反序列化应答负载
+    add_contact::AddContactResponse resp;
+    bool parse = resp.ParseFromString(res->body);
+    if (!parse) {
+        throw ContactsException(
+            format("/contacts/add 资源应答序列化失败, 会话层状态码: {}", res->status));
+    } else if (res->status != 200) {
+        // 2xx 是请求正常处理码, 不等于就是对端应用层异常
+        throw ContactsException(
+            format("/contacts/add 资源应答异常, 会话层状态码: {}, 服务端错误原因: {}", res->status,
+                   resp.error_desc()));
+    } else if (!resp.success()) {
+        // 对端调用结束, 但结果异常, 责任可能在于客户提交参数有误
+        throw ContactsException(format("/contacts/add 资源应答无法处理: {}", resp.error_desc()));
+    }
+
+    // 输出结果
+    cout << format("新增联系人成功, 联系人身份码为: {}\n", resp.uid());
 }
--- contacts4.0/src/server/main.cc
+++ contacts4.0/src/server/main.cc
@@ -1,22 +1,103 @@
 #include <iostream>
+#include <random>
+#include <string>
 
 #include "httplib.h"
+#include "proto/add_contact.pb.h"
 
 using namespace std;
 using namespace httplib;
+
+class ContactsException : public exception {
+   public:
+    ContactsException(std::string m, int s) : message(m), status(s) {}
+
+    const char* what() const noexcept override {
+        return message.c_str();
+    }
+
+    int code() const {
+        return status;
+    }
+
+   private:
+    std::string message;
+    int status;
+};
+
+// C++ 风格的随机数生成
+uint random_char() {
+    // 从系统提供的随机源取一个随机种子（有些平台是硬件级别真随机，有些是软件级时间伪随机）
+    std::random_device rd;
+    // 用这个种子初始化伪随机数引擎
+    std::mt19937 gen(rd());
+    // 为生成的结果进行处理映射, 从而约束在某一范围
+    std::uniform_int_distribution<> dis(0, 255);
+    // 返回生成的随机数
+    return dis(gen);
+}
+
+std::string generate_uid(uint len = 16) {
+    std::string s;
+    for (uint i = 0; i < len; ++i) {
+        int rc = random_char();
+        // 把结果格式化成十六进制形式, 且不足2位用0补齐
+        auto hex = format("{:02x}", rc);
+        s += hex;
+    }
+    return s;
+}
 
 int main() {
     cout << "---------服务启动---------" << endl;
     Server server;
 
-    server.Post("/test-post", [](const Request& req, Response& res) {
-        cout << "接收到post请求!" << endl;
-        res.status = 200;
-    });
+    server.Post("/contacts/add", [](const Request& req, Response& res) {
+        cout << "接收到post请求: /contacts/add" << endl;
 
-    server.Get("/test-get", [](const Request& req, Response& res) {
-        cout << "接收到get请求!" << endl;
-        res.status = 200;
+        add_contact::AddContactRequest request;
+        add_contact::AddContactResponse response;
+
+        try {
+            // 对负载进行反序列化
+            if (!request.ParseFromString(req.body)) {
+                // 4xx 表示服务端无法服务
+                throw ContactsException("客户请求无法反序列化!", 400);
+            }
+
+            // 不写进文件了, 因为我们没写 message contact, 为了方便, 就直接打印了
+            cout << "新增联系人↓\n";
+            cout << format("联系人姓名: {}\n", request.name());
+            cout << format("联系人年龄: {}\n", request.age());
+            int size = request.phones_size();
+            for (int i = 0; i < size; ++i) {
+                auto phone = request.phones(i);
+                cout << format("电话{} : {}\n", i + 1, phone);
+            }
+            cout << endl;
+
+            // 构造并序列化应答负载
+            response.set_success(true);
+            response.set_uid(generate_uid());
+
+            std::string body;
+            if (!response.SerializeToString(&body)) {
+                // 5xx 表示服务端处理请求出错
+                throw ContactsException("客户应答无法序列化!", 500);
+            }
+            res.body = body;
+
+        } catch (ContactsException& e) {
+            res.status = e.code();
+            response.set_success(false);
+            response.set_error_desc(e.what());
+            std::string body;
+            if (response.SerializeToString(&body)) {
+                res.body = body;
+                res.set_header("Content-Type", "application/protobuf");
+            }
+            cout << format("/contacts/add 应用级异常: {}\n", e.what());
+        }
     });
 
     server.listen("0.0.0.0", 8180);
